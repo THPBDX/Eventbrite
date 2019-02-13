8dde6c06ad882059973e804b0046feee04a0a970
Misa7 <ma.ducros7@gmail.com>
2019-02-14 00:17:20 +0100

    NTM LES CARDS

--- db/schema.rb
+++ db/schema.rb
@@ -52,4 +52,5 @@ ActiveRecord::Schema.define(version: 2019_02_12_154623) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "attendances", "events"
 end
